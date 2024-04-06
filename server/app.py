e0a50cba14620f0eccd572e1fbfaf2bcab2a22f0
Taihyun Lee <taihyunlee@Taihyuns-MacBook-Pro.local>
2024-04-06 19:28:21 -0400

    adding integration

--- server/app.py
+++ server/app.py
@@ -4,12 +4,12 @@ from gen_cover_letter import main
 
 st.set_page_config(page_title="Home Page", page_icon=":lemon:", layout="centered")
 
-hide_menu_style = "<style> footer {visibility: hidden;} </style>"
+hide_menu_style = "<style> footer {visibility: hidden;} textarea {width: -webkit-fill-available; height: 1200px;} </style>"
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
-st.title("👩🏼‍💻 Cover Letter Generator")
+st.title("📝 CoverCraft: Tailored Applications")
     
-st.info("Please enter your CV and the job description in the text areas below. Then click the submit button to generate your cover letter. (Reponse has a limit of 250 words)", icon='ℹ️')
+st.info("Please enter the requested information in the text areas below. Then click the submit button to generate your cover letter.", icon='ℹ️')
     
 with st.form("my_form"):
    st.write("Inside the form")
@@ -23,9 +23,7 @@ with st.form("my_form"):
    if submit:
        with st.spinner('Generating cover letter...'):
            download_button_html = main(jobDescription)
-           st.markdown(download_button_html, unsafe_allow_html=True)
-        #    text_output = main()
-        #    st.text_area(label='Cover Letter:',value=text_output)
+           st.markdown(f'<textarea>{download_button_html}</textarea>', unsafe_allow_html=True)
    
 
 
--- server/gen_cover_letter.py
+++ server/gen_cover_letter.py
@@ -14,19 +14,20 @@ from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.postprocessor import LLMRerank
 from fpdf import FPDF
 import base64
+import certifi
 
 from cover_letter_prompt import COVER_LETTER_PROMPT_TEMPLATE
 
 def hardcode_env_vars():
-    os.environ["ANTHROPIC_API_KEY"] = "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"
+    os.environ["ANTHROPIC_API_KEY"] = "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"
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     os.environ["MONGO_URI"] = "mongodb+srv://adas006:ayan@embeddingsdata.7urn3ch.mongodb.net/?retryWrites=true&w=majority&appName=EmbeddingsData"
 
 def initialize_global_settings():
-    os.environ["ANTHROPIC_API_KEY"] = "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"
+    # os.environ["ANTHROPIC_API_KEY"] = "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"
     Settings.llm = Anthropic(
-        model="claude-3-haiku-20240307"
-        # model="claude-3-opus-20240229"
+        # model="claude-3-haiku-20240307"
+        model="claude-3-opus-20240229"
     )
     Settings.embed_model = HuggingFaceEmbedding(
         model_name="BAAI/bge-small-en-v1.5"
@@ -48,12 +49,12 @@ def main(
 
     # Connect to database
     mongodb_client = pymongo.MongoClient(
-        os.getenv("MONGO_URI")
+        os.getenv("MONGO_URI"), tlsCAFile=certifi.where()
     )
     mongo_vector_store = MongoDBAtlasVectorSearch(
         mongodb_client,
         db_name="test-database",
-        collection_name="uber_docs",
+        collection_name="ayan_docs",
         index_name="default"
     )
     mongo_index = VectorStoreIndex.from_vector_store(
@@ -95,28 +96,7 @@ def main(
     )
 
     
-    pdf = FPDF()
- 
-    # Add a page
-    pdf.add_page()
-    
-    # set style and size of font 
-    # that you want in the pdf
-    pdf.set_font("Arial", size = 15)
-    
-    # create a cell
-    pdf.cell(200, 10, txt = "GeeksforGeeks", 
-            ln = 1, align = 'C')
-    
-    # add another cell
-    pdf.cell(200, 10, txt = cover_letter,
-            ln = 2, align = 'C')
-    
-    # save the pdf with name .pdf
-    # pdf.output("GFG.pdf") 
-    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
-
-    return html
+    return cover_letter
 
 if __name__ == "__main__":
     print(main())
